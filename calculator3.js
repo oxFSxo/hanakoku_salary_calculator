3174b25f6cc53d8e5f1f5f39f4be8dacae926b2d
oxFSxo <33600000+oxFSxo@users.noreply.github.com>
2018-05-12 22:17:57 +0900

    2018/05/12 長期契約対応

---
+++ calculator3.js
@@ -0,0 +1,11 @@
+﻿function calculate() {
+  var all = document.getElementById("all");
+  var period = document.getElementById("period");
+  if (all.value == "" || period.value == "") {
+    alert("未入力の欄があります");
+    return;
+  }
+  var next = document.getElementById("next");
+  var value1 = Math.ceil(all.value / 10000) * 8000;
+  next.value = value1;
+};
